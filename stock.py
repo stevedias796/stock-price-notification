3c4d040dfbf015bf687ddd8a16c5405cd5f6f9ef
stevedias796 <55200261+stevedias796@users.noreply.github.com>
2021-01-09 17:25:19 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pymongo
 import os
+from datetime import date, timedelta, datetime
 
 
 myClient = pymongo.MongoClient(os.environ['MONGO_CLIENT'])
@@ -11,8 +12,15 @@ myDb = myClient['stocksDB']
 # create collection
 myCollection = myDb['subscriptions']
 
-app = Flask(__name__)
+# create database
+my_Db = myClient['SGGoalsDB']
 
+#create collection
+my_Collection = my_Db['goals']
+myOtherCollection = my_Db['report']
+myGoal = my_Db['finalgoal']
+
+app = Flask(__name__)
 
 def send_stock_sms(stk_name, company_name, email, mobile, message):
     STOCK_NAME = stk_name
@@ -57,6 +65,69 @@ def submit():
         return render_template("stocks.html", message=mess)
     else:
         return render_template("stocks.html", message="")
+
+    
+@app.route('/home')
+def home():
+    finGoal = myGoal.find()
+    data = my_Collection.find()
+    return render_template('/khata.html', data=data, goal=finGoal)
+
+
+@app.route('/add-goal', methods=['POST'])
+def add_goal():
+    forgoal = request.form['forGoal']
+    amount = float(request.form['newGoal'])
+    data = my_Collection.find_one({'for': forgoal}, {'_id': 0, 'amount': 1})
+    print(data)
+    if data is not None:
+        print(data)
+        new_amount = amount + float(data['amount'])
+        myquery = {'for': forgoal}
+        newvalues = {"$set": {"amount": new_amount}}
+        my_Collection.update_one(myquery, newvalues)
+
+        report_data = {
+            'for': forgoal,
+            'amount': amount,
+            'date': datetime.today()
+        }
+        insert_report = myOtherCollection.insert_one(report_data)
+
+        newach_goal = {"$set": {"acheivedgoal": new_amount}}
+
+        myGoal.update_one({}, newach_goal)
+
+        return redirect("/home")
+
+    else:
+        new_data = {
+            'for': forgoal,
+            'amount': amount,
+        }
+        insert_data = my_Collection.insert_one(new_data)
+
+        if insert_data.inserted_id:
+            report_data = {
+                'for': forgoal,
+                'amount': amount,
+                'date': datetime.today()
+            }
+            insert_report = myOtherCollection.insert_one(report_data)
+
+            data = myGoal.find_one({}, {'_id': 0})
+            new_amount = float(data['acheivedgoal']) + amount
+            newvalues = {"$set": {"acheivedgoal": new_amount}}
+
+            myGoal.update_one({}, newvalues)
+
+        return redirect('/home')
+
+
+@app.route('/goal-report', methods=['GET','POST'])
+def report():
+    data = myOtherCollection.find()
+    return render_template('/goalReport.html', data=data)
 
 
 if __name__ == "__main__":
